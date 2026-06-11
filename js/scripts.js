6bf415a7ae330f8b82ca0d8ae903abb105fbee33
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-11 16:38:58 -0300

    Lógica de programação

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,99 @@
 console.log("teste")
+
+console.log('comando de saída')
+
+//COMENTARIO DE MULTIPLAS LINHAS
+
+/*COMENTÁRIO DE MÚLTIPLAS LINHAS,
+OU SEJA, POSSO DESCREVER ALGO
+NESSE TIPO DE COMENTÁRIO...
+*/
+
+//CRIANDO VARIÁVEIS
+let num = 50
+var num2 = 180
+const num3 = 450
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true) {
+    let num4 = 420
+    var num5 = 180
+
+console.log(num4)
+
+console.log(num)
+}
+
+console.log(num5)
+
+num = 5
+
+console.log(num)
+
+num2 = 6
+
+console.log(num2)
+
+//num3 = 12
+
+//CONCATENAÇÃO
+console.log("valor de num" + num)
+console.log('Valor de num', num3)
+console.log(`Valor de num2 ${num2}`)
+
+console.log(`A soma dos números ` + (15 + 8))
+console.log(`A soma dos números `, 15 + 8)
+console.log(`A soma dos números ${15 + 8}`)
+console.log(`A soma dos números ` + num + num2)
+console.log(`A soma dos números `, num2 + 8)
+console.log(`A soma dos números ${num + num2}`)
+
+//OPERADORES MATEMÁTICOS
+/*
+     + SOMA
+     - SUBTRAÇÃO
+     * MULTIPLICAÇÃO
+     / DIVISÃO
+*/
+
+console.log(`A soma das variáveis `, num + num2)
+console.log(`A soma das variáveis `, num3 - num)
+console.log(`Multiplicação `, num2 * 2)
+console.log(`Divisão de valores `, num2 / 5)
+console.log(`Resto da divisão `, num2 % 2)
+console.log(`Resto da divisão ${num % 3}`)
+
+
+//OPERADORES RELACIONAIS
+/*
+     == iGUAL
+     > MAIOR 
+     < MENOR
+     >= MAIOR IGUAL
+     <= MENOR IGUAL
+     != DIFERENTE
+ */
+
+
+//OPREADORES LÓGICOS 
+/*
+    && E
+    || OU
+*/
+
+console.log(`TESTE RELACIONAIS ` , 10 == 5)
+console.log(`TESTE RELACIONAIS ` , 10 != 5)
+console.log(`TESTE RELACIONAIS ` , 10 > 5)
+console.log(`TESTE RELACIONAIS ` ,10 < 5)
+console.log(`TESTE RELACIONAIS ` , 10 <= 5)
+console.log(`TESTE LÓGICO` , (10 > 5) && (15 > 8))
+console.log(`TESTE LÓGICO `, (10 > 5) || (15 < 8))
+
+
+
+
+
+
